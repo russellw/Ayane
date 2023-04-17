879a8256f368288bdc33ad206dbed092496c0460
Russell Wallace <russell.wallace@gmail.com>
2023-04-17 15:14:07 +0100

    fmt

--- etc/e-trace.py
+++ etc/e-trace.py
@@ -3,32 +3,36 @@ import os
 import re
 import subprocess
 
-parser = argparse.ArgumentParser(description="Run the E prover, tracing generated clauses")
+parser = argparse.ArgumentParser(
+    description="Run the E prover, tracing generated clauses"
+)
 parser.add_argument("files", nargs="+")
 args = parser.parse_args()
 
-os.makedirs('logs-e',exist_ok=True)
+os.makedirs("logs-e", exist_ok=True)
 
-def replace_dir( file,d):
-    file=os.path.basename(file)
-    return os.path.join(d,file)
-def replace_ext(file,ext)    :
+
+def replace_dir(file, d):
+    file = os.path.basename(file)
+    return os.path.join(d, file)
+
+
+def replace_ext(file, ext):
     file = os.path.splitext(file)[0]
-    return file+ext
+    return file + ext
+
 
 def do(file):
     print(file)
     # --auto makes a big difference to performance
     # don't use --auto-schedule
     # for some reason, it breaks the timeout feature
-    cmd = "E/PROVER/eprover", "--auto", "-l",'1', file
-    file=replace_dir(file,'logs-e')
-    file=replace_ext(file,'.log')
-    out=open(file,'w')
+    cmd = "E/PROVER/eprover", "--auto", "-l", "1", file
+    file = replace_dir(file, "logs-e")
+    file = replace_ext(file, ".log")
+    out = open(file, "w")
     try:
-        p = subprocess.run(
-            cmd,  check=True,stdout=out,  timeout=3
-        )
+        p = subprocess.run(cmd, check=True, stdout=out, timeout=3)
     except subprocess.TimeoutExpired:
         pass
 
