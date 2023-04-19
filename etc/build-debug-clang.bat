e3cb433fc38b87f282328187573d47932da31f2a
Russell Wallace <russell.wallace@gmail.com>
2023-04-19 14:12:03 +0100

    build with clang

---
+++ etc/build-debug-clang.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\LLVM\bin\clang-cl" /DDEBUG /IC:\mpir /std:c++17 -Werror -Wimplicit-fallthrough -Wno-assume -Wno-deprecated-declarations -Wno-switch -c -ferror-limit=1 C:\ayane\src\*.cc
--- etc/build-debug.bat
+++ etc/build-debug.bat
@@ -1,8 +1,2 @@
-if "%VCINSTALLDIR%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-rem Try compiling with clang first, to get a second opinion on warnings/errors
-rem clang-cl /DDEBUG /IC:\mpir /std:c++17 -Werror -Wimplicit-fallthrough -Wno-assume -Wno-deprecated-declarations -Wno-switch -c -ferror-limit=1 C:\ayane\src\*.cc
-rem if errorlevel 1 goto :eof
-
-rem Then compile with Microsoft C++
+if "%VCINSTALLDIR%"=="" call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 cl /DDEBUG /Feayane /IC:\mpir /MP /MTd /Os /WX /Zi /std:c++17 C:\ayane\src\*.cc C:\mpir\debug.lib dbghelp.lib
