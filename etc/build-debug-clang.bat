565bc8552d92adb3a122460370d0800258fe60cb
Russell Wallace <russell.wallace@gmail.com>
2023-04-19 14:52:23 +0100

    rearrange

--- etc/build-debug-clang.bat
+++ etc/build-debug-clang.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\LLVM\bin\clang-cl" /DDEBUG /Feayane /IC:\mpir /std:c++17 -Wimplicit-fallthrough -Wno-deprecated-declarations -Wno-switch -ferror-limit=1 C:\ayane\src\*.cc C:\mpir\debug.lib dbghelp.lib
+"C:\Program Files\LLVM\bin\clang-cl" /DDEBUG /Feayane /IC:\mpir /std:c++17 -Wimplicit-fallthrough -Wno-deprecated-declarations -Wno-switch -ferror-limit=1 C:\ayane\*.cc C:\mpir\debug.lib dbghelp.lib
--- etc/build-release.bat
+++ etc/build-release.bat
@@ -1,2 +1,2 @@
 if "%VCINSTALLDIR%"=="" call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-cl /Feayane /IC:\mpir /O2 /std:c++17 C:\ayane\src\*.cc C:\mpir\release.lib
+cl /Feayane /IC:\mpir /O2 /std:c++17 C:\ayane\*.cc C:\mpir\release.lib
