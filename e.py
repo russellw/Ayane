873a536347daa8b75c8709da19ec3b2bfb2541b4
Russell Wallace <russell.wallace@gmail.com>
2023-04-23 11:27:42 +0100

    refactor

--- e.py
+++ e.py
@@ -1,11 +1,62 @@
 import argparse
 import os
+import random
 import re
 import subprocess
+import time
 
-parser = argparse.ArgumentParser(description="Run the E prover on a batch of problems")
-parser.add_argument("files", nargs="+")
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-n", "--number", help="max number of problems to attempt", type=int
+)
+parser.add_argument(
+    "-r", "--random", help="attempt problems in random order", action="store_true"
+)
+parser.add_argument("-s", "--seed", help="random number seed", type=int)
+parser.add_argument(
+    "-t", "--time", help="time limit per problem", type=float, default=60.0
+)
+parser.add_argument("files", nargs="*")
 args = parser.parse_args()
+
+if args.seed is not None:
+    args.random = 1
+    random.seed(args.seed)
+if not args.files:
+    args.files = ["tptp"]
+
+tptp = os.getenv("TPTP")
+if not tptp:
+    raise Exception("TPTP environment variable not set")
+
+problems = []
+for arg in args.files:
+    if arg.lower() == "tptp":
+        arg = tptp
+    elif re.match(r"[A-Za-z][A-Za-z][A-Za-z]$", arg):
+        arg = arg.upper()
+        arg = os.path.join(tptp, "Problems", arg)
+    elif re.match(r"[A-Za-z][A-Za-z][A-Za-z]\d\d\d.\d+$", arg):
+        arg = arg.upper()
+        arg = os.path.join(tptp, "Problems", arg[:3], arg + ".p")
+
+    if os.path.isdir(arg):
+        for root, dirs, files in os.walk(arg):
+            for file in files:
+                ext = os.path.splitext(file)[1]
+                if ext == ".p" and "^" not in file:
+                    problems.append(os.path.join(root, file))
+        continue
+    if arg.endswith(".lst"):
+        for s in open(arg):
+            if "^" not in s:
+                problems.append(s.rstrip())
+        continue
+    problems.append(arg)
+if args.random:
+    random.shuffle(problems)
+if args.number:
+    problems = problems[0 : args.number]
 
 
 def difficulty(file):
@@ -16,42 +67,30 @@ def difficulty(file):
     return "?"
 
 
-def do(file):
-    print(file, end=",", flush=True)
-    print(difficulty(file), end=",", flush=True)
+for file in problems:
+    print(os.path.basename(file), end="\t")
+    print(difficulty(file), end="\t", flush=True)
+
     # --auto makes a big difference to performance
     # don't use --auto-schedule
     # for some reason, it breaks the subprocess timeout feature
     cmd = "bin/eprover", "--auto", "-p", file
+
+    t = time.time()
     try:
         p = subprocess.run(
-            cmd, capture_output=True, check=True, encoding="utf-8", timeout=3
+            cmd, capture_output=True, encoding="utf-8", timeout=args.time
         )
-        print(len(p.stdout.splitlines()), end=",", flush=True)
-        if "Proof found" in p.stdout:
-            r = "Unsatisfiable"
-        elif "No proof found":
-            r = "Satisfiable"
-        else:
-            raise Exception(p.stdout)
+        if p.returncode == 3:
+            print()
+            continue
+
+        print("%0.3f" % (time.time() - t), end="\t")
+        print(len(p.stdout.splitlines()), end="\t")
+        m = re.search(r"SZS status (\w+)", p.stdout)
+        r = m[1]
     except subprocess.TimeoutExpired:
-        print(0, end=",", flush=True)
+        print("%0.3f" % (time.time() - t), end="\t")
+        print(0, end="\t")
         r = "Timeout"
-    print(r, flush=True)
-
-
-for arg in args.files:
-    if os.path.isdir(arg):
-        for root, dirs, files in os.walk(arg):
-            for file in files:
-                ext = os.path.splitext(file)[1]
-                if ext != ".p":
-                    continue
-                do(os.path.join(root, file))
-        continue
-    ext = os.path.splitext(arg)[1]
-    if ext == ".lst":
-        for file in open(arg):
-            do(file.strip())
-        continue
-    do(arg)
+    print(r)
