7b2d785c637a9e1f57d39d28340a66b8346f2c0f
Russell Wallace <russell.wallace@gmail.com>
2023-04-28 20:45:33 +0100

    refactor

--- unify.h
+++ unify.h
@@ -1,3 +1,3 @@
 bool match(Term* a, Term* b, vec<pair<Term*, Term*>>& m);
-bool unify(Term* a, bool ax, Term* b, bool bx, vec<pair<Termx, Termx>>& m);
-Term* replace(Term* a, bool ax, const vec<pair<Termx, Termx>>& m);
+bool unify(Term* a, bool ax, Term* b, bool bx, vec<pair<termx, termx>>& m);
+Term* replace(Term* a, bool ax, const vec<pair<termx, termx>>& m);
