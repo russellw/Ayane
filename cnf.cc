bfe43c88983180f0d6c8ded038a18f9be24111e5
Russell Wallace <russell.wallace@gmail.com>
2023-04-28 21:02:08 +0100

    unwrap

--- cnf.cc
+++ cnf.cc
@@ -81,292 +81,283 @@ size_t ncsApprox(int pol, Term* a) {
 	return n;
 }
 
-struct doing {
-	// SORT
-	set<clause>& cs;
-	set<Term*> defs;
-	ProofCnf& proofCnf;
-	size_t vars = 0;
-	///
+// Skolem functions replace existentially quantified variables, also formulas that are renamed to avoid exponential expansion
+Term* skolem(type rty, const vec<Term*>& args) {
+	vec<Term*> v(1, gensym(ftype(rty, args)));
+	// TODO: single call instead of loop?
+	for (auto b: args) v.push_back(b);
+	return mk(v);
+}
 
-	// Skolem functions replace existentially quantified variables, also formulas that are renamed to avoid exponential expansion
-	Term* skolem(type rty, const set<Term*>& args) {
-		vec<Term*> v(1, gensym(ftype(rty, args)));
-		// TODO: single call instead of loop?
-		for (auto b: args) v.push_back(b);
+// SORT
+vec<Term*> defs;
+size_t vars = 0;
+///
+
+// Rename formulas to avoid exponential expansion. It's tricky to do this while in the middle of doing other things, easier to be
+// sure of the logic if it's done as a separate pass first.
+Term* rename(int pol, Term* a) {
+	auto b = skolem(getType(a), freeVars(a));
+	// NO_SORT
+	switch (pol) {
+	case 1:
+		// If this formula is only being used with positive polarity, the new name only needs to imply the original formula
+		a = imp(b, a);
+		break;
+	case -1:
+		// And the reverse for negative polarity
+		a = imp(a, b);
+		break;
+	case 0:
+		// In the general case, full equivalence is needed; the new name implies and is implied by the original formula
+		a = mk(And, imp(b, a), imp(a, b));
+		break;
+	default:
+		unreachable;
+	}
+	defs.add(quantify(a));
+	return b;
+}
+
+// Maybe rename some of the arguments to an OR-over-AND (taking polarity into account), where the number of clauses generated would
+// be the product of the arguments
+void maybeRename(int pol, vec<Term*>& v) {
+	// Sorting the arguments doesn't change the meaning of the formula, because AND and OR are commutative. The effect is that if
+	// only some of them are to be renamed, we will leave the simple ones alone and end up renaming the complicated ones, which is
+	// probably what we want.
+	sort(v.begin() + 1, v.end(), [=](Term* a, Term* b) { return ncsApprox(pol, a) < ncsApprox(pol, b); });
+	size_t n = 1;
+	for (size_t i = 1; i != v.size(); ++i) {
+		auto m = ncsApprox(pol, v[i]);
+		if (n * m < many) n *= m;
+		else
+			v[i] = rename(pol, v[i]);
+	}
+}
+
+// Given a formula, and whether it is used for positive polarity, negative or both (i.e. under an equivalence), maybe rename some of
+// its subformulas. If a subformula occurs many times (whether under the same formula, or different ones), it is considered in
+// isolation each time, so that each occurrence could end up with a different name. In principle, it would be more efficient to
+// rename on a global basis, but in practice, nontrivial subformulas are rarely duplicated (e.g. less than 1% of the nontrivial
+// formulas in the TPTP), so this is probably not worth doing.
+Term* maybeRename(int pol, Term* a) {
+	vec<Term*> v(1, at(a, 0));
+	// NO_SORT
+	switch (a->tag) {
+	case All:
+	case Exists:
+		v.push_back(maybeRename(pol, at(a, 1)));
+		for (size_t i = 2; i < a->n; ++i) v.push_back(at(a, i));
+		break;
+
+	case Not:
+		return mk(Not, maybeRename(-pol, at(a, 1)));
+
+	case Or:
+		for (size_t i = 1; i < a->n; ++i) v.push_back(maybeRename(pol, at(a, i)));
+
+		// If this formula will be used with positive polarity (including the case where it will be used both ways), we are looking
+		// at OR over possible ANDs, which would produce exponential expansion at the distribution stage, so may need to rename some
+		// of the arguments
+		if (pol >= 0) maybeRename(pol, v);
+		break;
+	case And:
+		for (size_t i = 1; i < a->n; ++i) v.push_back(maybeRename(pol, at(a, i)));
+
+		// NOT-AND yields OR, so mirror the OR case
+		if (pol <= 0) maybeRename(pol, v);
+		break;
+
+	case Eqv:
+	{
+		auto x = maybeRename(0, at(a, 1));
+		auto y = maybeRename(0, at(a, 2));
+		if (ncsApprox(0, x) >= many) x = rename(0, x);
+		if (ncsApprox(0, y) >= many) y = rename(0, y);
+		return mk(Eqv, x, y);
+	}
+
+	default:
+		return a;
+	}
+	return mk(v);
+}
+
+// For-all doesn't need much work to convert. Clauses contain variables with implied for-all. The tricky part is that quantifier
+// binds variables to local scope, so the same variable name used in two for-all's corresponds to two different logical variables.
+// So we rename each quantified variable to a new variable of the same type.
+map<Term*, Term*> all(map<Term*, Term*> m, Term* a) {
+	for (size_t i = 2; i < a->n; ++i) {
+		auto x = at(a, i);
+		assert(x->tag == Var);
+		auto y = var(vars++, ((Atom*)x)->ty);
+		m.add(x, y);
+	}
+	return m;
+}
+
+// Each existentially quantified variable is replaced with a Skolem function whose parameters are all the surrounding universally
+// quantified variables
+map<Term*, Term*> exists(map<Term*, Term*> m, Term* a) {
+	// Get the surrounding universally quantified variables that will be arguments to the Skolem functions
+	vec<Term*> args;
+	for (auto& kv: m)
+		if (kv.second->tag == Var) args.add(kv.second);
+
+	// Make a replacement for each existentially quantified variable
+	for (size_t i = 2; i < a->n; ++i) {
+		auto x = at(a, i);
+		assert(x->tag == Var);
+		auto y = skolem(((Atom*)x)->ty, args);
+		m.add(x, y);
+	}
+	return m;
+}
+
+// Negation normal form consists of several transformations that are as easy to do at the same time: Move NOTs inward to the literal
+// layer, flipping things around on the way, while simultaneously resolving quantifiers
+Term* nnf(map<Term*, Term*> m, bool pol, Term* a) {
+	vec<Term*> v(1, at(a, 0));
+	// NO_SORT
+	switch (a->tag) {
+		// Boolean constants and operators can be inverted by downward-sinking NOTs
+	case False:
+		return mkbool(!pol);
+	case True:
+		return mkbool(pol);
+
+	case Not:
+		return nnf(m, !pol, at(a, 1));
+
+	case Or:
+		if (!pol) v[0] = mk(And);
+		for (size_t i = 1; i < a->n; ++i) v.push_back(nnf(m, pol, at(a, i)));
 		return mk(v);
-	}
-
-	// Rename formulas to avoid exponential expansion. It's tricky to do this while in the middle of doing other things, easier to
-	// be sure of the logic if it's done as a separate pass first.
-	Term* rename(int pol, Term* a) {
-		auto b = skolem(getType(a), freeVars(a));
-		// NO_SORT
-		switch (pol) {
-		case 1:
-			// If this formula is only being used with positive polarity, the new name only needs to imply the original formula
-			a = imp(b, a);
-			break;
-		case -1:
-			// And the reverse for negative polarity
-			a = imp(a, b);
-			break;
-		case 0:
-			// In the general case, full equivalence is needed; the new name implies and is implied by the original formula
-			a = mk(And, imp(b, a), imp(a, b));
-			break;
-		default:
-			unreachable;
-		}
-		defs.add(quantify(a));
-		return b;
-	}
-
-	// Maybe rename some of the arguments to an OR-over-AND (taking polarity into account), where the number of clauses generated
-	// would be the product of the arguments
-	void maybeRename(int pol, vec<Term*>& v) {
-		// Sorting the arguments doesn't change the meaning of the formula, because AND and OR are commutative. The effect is that
-		// if only some of them are to be renamed, we will leave the simple ones alone and end up renaming the complicated ones,
-		// which is probably what we want.
-		sort(v.begin() + 1, v.end(), [=](Term* a, Term* b) { return ncsApprox(pol, a) < ncsApprox(pol, b); });
-		size_t n = 1;
-		for (size_t i = 1; i != v.size(); ++i) {
-			auto m = ncsApprox(pol, v[i]);
-			if (n * m < many) n *= m;
-			else
-				v[i] = rename(pol, v[i]);
-		}
-	}
-
-	// Given a formula, and whether it is used for positive polarity, negative or both (i.e. under an equivalence), maybe rename
-	// some of its subformulas. If a subformula occurs many times (whether under the same formula, or different ones), it is
-	// considered in isolation each time, so that each occurrence could end up with a different name. In principle, it would be more
-	// efficient to rename on a global basis, but in practice, nontrivial subformulas are rarely duplicated (e.g. less than 1% of
-	// the nontrivial formulas in the TPTP), so this is probably not worth doing.
-	Term* maybeRename(int pol, Term* a) {
-		vec<Term*> v(1, at(a, 0));
-		// NO_SORT
-		switch (a->tag) {
-		case All:
-		case Exists:
-			v.push_back(maybeRename(pol, at(a, 1)));
-			for (size_t i = 2; i < a->n; ++i) v.push_back(at(a, i));
-			break;
-
-		case Not:
-			return mk(Not, maybeRename(-pol, at(a, 1)));
-
-		case Or:
-			for (size_t i = 1; i < a->n; ++i) v.push_back(maybeRename(pol, at(a, i)));
-
-			// If this formula will be used with positive polarity (including the case where it will be used both ways), we are
-			// looking at OR over possible ANDs, which would produce exponential expansion at the distribution stage, so may need to
-			// rename some of the arguments
-			if (pol >= 0) maybeRename(pol, v);
-			break;
-		case And:
-			for (size_t i = 1; i < a->n; ++i) v.push_back(maybeRename(pol, at(a, i)));
-
-			// NOT-AND yields OR, so mirror the OR case
-			if (pol <= 0) maybeRename(pol, v);
-			break;
-
-		case Eqv:
-		{
-			auto x = maybeRename(0, at(a, 1));
-			auto y = maybeRename(0, at(a, 2));
-			if (ncsApprox(0, x) >= many) x = rename(0, x);
-			if (ncsApprox(0, y) >= many) y = rename(0, y);
-			return mk(Eqv, x, y);
-		}
-
-		default:
-			return a;
-		}
+	case And:
+		if (!pol) v[0] = mk(Or);
+		for (size_t i = 1; i < a->n; ++i) v.push_back(nnf(m, pol, at(a, i)));
 		return mk(v);
+
+		// Variables are mapped to new variables or Skolem functions
+	case Var:
+		return m.at(a);
+
+		// According to whether they are bound by universal or existential quantifiers
+	case All:
+		m = pol ? all(m, a) : exists(m, a);
+		return nnf(m, pol, at(a, 1));
+	case Exists:
+		m = pol ? exists(m, a) : all(m, a);
+		return nnf(m, pol, at(a, 1));
+
+		// Equivalence is the most difficult operator to deal with
+	case Eqv:
+	{
+		auto x = at(a, 1);
+		auto y = at(a, 2);
+		auto x0 = nnf(m, 0, x);
+		auto x1 = nnf(m, 1, x);
+		auto y0 = nnf(m, 0, y);
+		auto y1 = nnf(m, 1, y);
+		return pol ? mk(And, mk(Or, x0, y1), mk(Or, x1, y0)) : mk(And, mk(Or, x0, y0), mk(Or, x1, y1));
 	}
-
-	// For-all doesn't need much work to convert. Clauses contain variables with implied for-all. The tricky part is that quantifier
-	// binds variables to local scope, so the same variable name used in two for-all's corresponds to two different logical
-	// variables. So we rename each quantified variable to a new variable of the same type.
-	map<Term*, Term*> all(map<Term*, Term*> m, Term* a) {
-		for (size_t i = 2; i < a->n; ++i) {
-			auto x = at(a, i);
-			assert(x->tag == Var);
-			auto y = var(vars++, ((Atom*)x)->ty);
-			m.add(x, y);
-		}
-		return m;
 	}
+	for (size_t i = 1; i < a->n; ++i) v.push_back(nnf(m, 1, at(a, i)));
+	a = mk(v);
+	return pol ? a : mk(Not, a);
+}
 
-	// Each existentially quantified variable is replaced with a Skolem function whose parameters are all the surrounding
-	// universally quantified variables
-	map<Term*, Term*> exists(map<Term*, Term*> m, Term* a) {
-		// Get the surrounding universally quantified variables that will be arguments to the Skolem functions
-		set<Term*> args;
-		for (auto& kv: m)
-			if (kv.second->tag == Var) args.add(kv.second);
+// Distribute OR down into AND, completing the layering of the operators for CNF. This is the second place where exponential
+// expansion would occur, had selected formulas not already been renamed.
+Term* distribute(Term* a) {
+	vec<Term*> v(1, mk(And));
+	switch (a->tag) {
+	case And:
+		for (size_t i = 1; i < a->n; ++i) v.push_back(distribute(at(a, i)));
+		break;
+	case Or:
+	{
+		// Arguments can be taken without loss of generality as ANDs
+		vec<vec<Term*>> ands;
+		for (size_t i = 1; i < a->n; ++i) {
+			// Recur
+			auto b = distribute(at(a, i));
 
-		// Make a replacement for each existentially quantified variable
-		for (size_t i = 2; i < a->n; ++i) {
-			auto x = at(a, i);
-			assert(x->tag == Var);
-			auto y = skolem(((Atom*)x)->ty, args);
-			m.add(x, y);
+			// And make a flat layer of ANDs
+			ands.push_back(flatten(And, b));
 		}
-		return m;
+
+		// OR distributes over AND by Cartesian product
+		// TODO: can this be done by reference?
+		for (auto u: cartProduct(ands)) {
+			u.insert(u.begin(), mk(Or));
+			v.push_back(mk(u));
+		}
+		break;
 	}
-
-	// Negation normal form consists of several transformations that are as easy to do at the same time: Move NOTs inward to the
-	// literal layer, flipping things around on the way, while simultaneously resolving quantifiers
-	Term* nnf(map<Term*, Term*> m, bool pol, Term* a) {
-		vec<Term*> v(1, at(a, 0));
-		// NO_SORT
-		switch (a->tag) {
-			// Boolean constants and operators can be inverted by downward-sinking NOTs
-		case False:
-			return mkbool(!pol);
-		case True:
-			return mkbool(pol);
-
-		case Not:
-			return nnf(m, !pol, at(a, 1));
-
-		case Or:
-			if (!pol) v[0] = mk(And);
-			for (size_t i = 1; i < a->n; ++i) v.push_back(nnf(m, pol, at(a, i)));
-			return mk(v);
-		case And:
-			if (!pol) v[0] = mk(Or);
-			for (size_t i = 1; i < a->n; ++i) v.push_back(nnf(m, pol, at(a, i)));
-			return mk(v);
-
-			// Variables are mapped to new variables or Skolem functions
-		case Var:
-			return m.at(a);
-
-			// According to whether they are bound by universal or existential quantifiers
-		case All:
-			m = pol ? all(m, a) : exists(m, a);
-			return nnf(m, pol, at(a, 1));
-		case Exists:
-			m = pol ? exists(m, a) : all(m, a);
-			return nnf(m, pol, at(a, 1));
-
-			// Equivalence is the most difficult operator to deal with
-		case Eqv:
-		{
-			auto x = at(a, 1);
-			auto y = at(a, 2);
-			auto x0 = nnf(m, 0, x);
-			auto x1 = nnf(m, 1, x);
-			auto y0 = nnf(m, 0, y);
-			auto y1 = nnf(m, 1, y);
-			return pol ? mk(And, mk(Or, x0, y1), mk(Or, x1, y0)) : mk(And, mk(Or, x0, y0), mk(Or, x1, y1));
-		}
-		}
-		for (size_t i = 1; i < a->n; ++i) v.push_back(nnf(m, 1, at(a, i)));
-		a = mk(v);
-		return pol ? a : mk(Not, a);
+	default:
+		return a;
 	}
+	return mk(v);
+}
 
-	// Distribute OR down into AND, completing the layering of the operators for CNF. This is the second place where exponential
-	// expansion would occur, had selected formulas not already been renamed.
-	Term* distribute(Term* a) {
-		vec<Term*> v(1, mk(And));
-		switch (a->tag) {
-		case And:
-			for (size_t i = 1; i < a->n; ++i) v.push_back(distribute(at(a, i)));
-			break;
-		case Or:
-		{
-			// Arguments can be taken without loss of generality as ANDs
-			vec<vec<Term*>> ands;
-			for (size_t i = 1; i < a->n; ++i) {
-				// Recur
-				auto b = distribute(at(a, i));
-
-				// And make a flat layer of ANDs
-				ands.push_back(flatten(And, b));
-			}
-
-			// OR distributes over AND by Cartesian product
-			// TODO: can this be done by reference?
-			for (auto u: cartProduct(ands)) {
-				u.insert(u.begin(), mk(Or));
-				v.push_back(mk(u));
-			}
-			break;
-		}
-		default:
-			return a;
-		}
-		return mk(v);
+// Convert a suitably rearranged term into actual clauses
+void clauseTerm(Term* a, vec<Term*>& neg, vec<Term*>& pos) {
+	switch (a->tag) {
+	case All:
+	case And:
+	case Eqv:
+	case Exists:
+		unreachable;
+	case Not:
+		neg.push_back(at(a, 1));
+		return;
+	case Or:
+		for (size_t i = 1; i < a->n; ++i) clauseTerm(at(a, i), neg, pos);
+		return;
 	}
+	pos.push_back(a);
+}
 
-	// Convert a suitably rearranged term into actual clauses
-	void clauseTerm(Term* a, vec<Term*>& neg, vec<Term*>& pos) {
-		switch (a->tag) {
-		case All:
-		case And:
-		case Eqv:
-		case Exists:
-			unreachable;
-		case Not:
-			neg.push_back(at(a, 1));
-			return;
-		case Or:
-			for (size_t i = 1; i < a->n; ++i) clauseTerm(at(a, i), neg, pos);
-			return;
-		}
-		pos.push_back(a);
+clause clauseTerm(Term* a) {
+	vec<Term*> neg, pos;
+	clauseTerm(a, neg, pos);
+	auto c = make_pair(neg, pos);
+	c = simplify(map<Term*, Term*>(), c);
+	return c;
+}
+
+// And record the clauses
+void csTerm(Term* from, Term* a) {
+	for (auto& b: flatten(And, a)) {
+		auto c = clauseTerm(b);
+		if (c == truec) continue;
+		proofCnf.add(c, from);
+		cs.add(c);
 	}
-
-	clause clauseTerm(Term* a) {
-		vec<Term*> neg, pos;
-		clauseTerm(a, neg, pos);
-		auto c = make_pair(neg, pos);
-		c = simplify(map<Term*, Term*>(), c);
-		return c;
-	}
-
-	// And record the clauses
-	void csTerm(Term* from, Term* a) {
-		for (auto& b: flatten(And, a)) {
-			auto c = clauseTerm(b);
-			if (c == truec) continue;
-			proofCnf.add(c, from);
-			cs.add(c);
-		}
-	}
-
-	// Top level
-	doing(const set<Term*>& initialFormulas, ProofCnf& proofCnf, set<clause>& cs): proofCnf(proofCnf), cs(cs) {
-		// First run each input formula through the full process: Rename subformulas where necessary to avoid exponential expansion,
-		// then convert to negation normal form, distribute OR into AND, and convert to clauses
-		for (auto a: initialFormulas) {
-			auto from = a;
-			a = maybeRename(1, a);
-			vars = 0;
-			a = nnf(map<Term*, Term*>(), 1, a);
-			a = distribute(a);
-			csTerm(from, a);
-		}
-
-		// Then convert all the definitions created by the renaming process. That process works by bottom-up recursion, which means
-		// each renamed subformula is simple, so there is no need to put the definitions through the renaming process again; they
-		// just need to go through the rest of the conversion steps.
-		for (auto a: defs) {
-			auto from = a;
-			vars = 0;
-			a = nnf(map<Term*, Term*>(), 1, a);
-			a = distribute(a);
-			csTerm(from, a);
-		}
-	}
-};
+}
 } // namespace
 
-void cnf(const set<Term*>& initialFormulas, ProofCnf& proofCnf, set<clause>& cs) {
-	doing _(initialFormulas, proofCnf, cs);
+void cnf(Formula* formula) {
+	// First run each input formula through the full process: Rename subformulas where necessary to avoid exponential expansion,
+	// then convert to negation normal form, distribute OR into AND, and convert to clauses
+	for (auto a: initialFormulas) {
+		auto from = a;
+		a = maybeRename(1, a);
+		vars = 0;
+		a = nnf(map<Term*, Term*>(), 1, a);
+		a = distribute(a);
+		csTerm(from, a);
+	}
+
+	// Then convert all the definitions created by the renaming process. That process works by bottom-up recursion, which means each
+	// renamed subformula is simple, so there is no need to put the definitions through the renaming process again; they just need
+	// to go through the rest of the conversion steps.
+	for (auto a: defs) {
+		auto from = a;
+		vars = 0;
+		a = nnf(map<Term*, Term*>(), 1, a);
+		a = distribute(a);
+		csTerm(from, a);
+	}
 }
