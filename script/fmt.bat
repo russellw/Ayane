305e99dc894801b3c6742d595dc381c7691f1a6c
Russell Wallace <russell.wallace@gmail.com>
2023-05-09 07:02:03 +0100

    rearrange

--- script/fmt.bat
+++ script/fmt.bat
@@ -1,13 +1,13 @@
-black .
+black %~dp0
 if %errorlevel% neq 0 goto :eof
 
-isort .
+isort %~dp0
 if %errorlevel% neq 0 goto :eof
 
-"C:\Program Files\LLVM\bin\clang-format" -i -style=file *.cc *.h
+"C:\Program Files\LLVM\bin\clang-format" -i -style=file %~dp0..\src\*.cc %~dp0..\src\*.h
 if %errorlevel% neq 0 goto :eof
 
-python fmt-c.py .
+python fmt-c.py %~dp0..\src
 if %errorlevel% neq 0 goto :eof
 
 git diff
