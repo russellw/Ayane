22331ded4a36c26e7e38ace8fe400d180603852e
Russell Wallace <russell.wallace@gmail.com>
2023-05-10 22:33:32 +0100

    test

---
+++ test/test.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+
+here = os.path.dirname(os.path.realpath(__file__))
+src = os.path.join(here, "..", "src",'*.cc')
+
+cmd='cl', '/DDBG', '/Feayane', '/IC:/mpir', '/MP', '/MTd', '/WX', '/Zi' ,'/std:c++17', src, 'C:/mpir/debug.lib', 'dbghelp.lib'
+subprocess.check_call(cmd)
