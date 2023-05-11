ab1f346382b4298958f5eadedd403d2e7f2ead0f
Russell Wallace <russell.wallace@gmail.com>
2023-05-11 06:14:42 +0100

    test

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 
 here = os.path.dirname(os.path.realpath(__file__))
@@ -19,3 +20,38 @@ cmd = (
     "dbghelp.lib",
 )
 subprocess.check_call(cmd)
+print()
+
+problems = []
+for root, dirs, files in os.walk(here):
+    for file in files:
+        ext = os.path.splitext(file)[1]
+        if ext in (".p", ".cnf"):
+            problems.append(os.path.join(root, file))
+
+for file in problems:
+    print(file)
+
+    e = None
+    for s in open(file).readlines():
+        m = re.match(r"\S\s*unsat\s+", s)
+        if m:
+            e = 0
+            break
+
+        m = re.match(r"\S\s*sat\s+", s)
+        if m:
+            e = 1
+            break
+    assert e is not None
+
+    cmd = "ayane", "-t3", file
+    s = subprocess.check_output(cmd, encoding="utf-8")
+    r = None
+    if "unsat" in s:
+        r = 0
+    elif "sat" in s:
+        r = 1
+    else:
+        raise Exception(s)
+    assert r == e
