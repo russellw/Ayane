9b7c524452c0181185accb3525457fee05bcbf09
Russell Wallace <russell.wallace@gmail.com>
2023-05-12 04:30:22 +0100

    clean

--- script/build-debug-clang.bat
+++ script/build-debug-clang.bat
@@ -1,2 +1,1 @@
-rem TODO: still need no-switch?
 "C:\Program Files\LLVM\bin\clang-cl" /DDBG /Feayane /IC:\mpir /std:c++17 -Wimplicit-fallthrough -Wno-deprecated-declarations -Wno-switch -ferror-limit=1 %~dp0..\src\*.cc C:\mpir\debug.lib dbghelp.lib
