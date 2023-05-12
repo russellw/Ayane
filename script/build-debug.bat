09de2e1fde9eb500c4f16c863ddfcb5b92aa78c1
Russell Wallace <russell.wallace@gmail.com>
2023-05-12 04:48:14 +0100

    clean

--- script/build-debug.bat
+++ script/build-debug.bat
@@ -1,4 +1,3 @@
 cl /DDBG /Feayane /IC:\mpir /MP /MTd /WX /Zi /std:c++17 %~dp0..\src\*.cc C:\mpir\debug.lib dbghelp.lib
-rem TODO: check additional warnings with all three compilers
 rem TODO: also check sanitizers https://nullprogram.com/blog/2023/04/29/
 rem TODO: check -g3
