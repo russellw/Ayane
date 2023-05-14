5e398d59c6ac2c10712d15e3b666c98b15395f32
Russell Wallace <russell.wallace@gmail.com>
2023-05-14 10:12:37 +0100

    vtune

--- script/vtun.bat
+++ script/vtun.bat
@@ -1,3 +1,6 @@
 if [%~1]==[] goto :eof
-cl /Feayane /IC:\mpir /MTd /O2 /Zi /std:c++17 %~dp0..\src\*.cc C:\mpir\debug.lib dbghelp.lib
+
+cl /Feayane /IC:\mpir /MP /MTd /Ox /WX /Zi /std:c++17 %~dp0..\src\*.cc C:\mpir\debug.lib dbghelp.lib
+if %errorlevel% neq 0 goto :eof
+
 "C:\Program Files (x86)\Intel\oneAPI\vtune\latest\bin64\vtune" -collect hotspots -user-data-dir %tmp% ayane.exe %*
