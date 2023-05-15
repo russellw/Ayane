e7bee633e81d2e6b6e6640d5f82a5d21aba6329e
Russell Wallace <russell.wallace@gmail.com>
2023-05-15 04:35:43 +0100

    first use clang to check for errors, as its diagnostics are slightly better

--- script/cmp_asm.py
+++ script/cmp_asm.py
@@ -24,20 +24,27 @@ def count_lines(file):
 
 here = os.path.dirname(os.path.realpath(__file__))
 
+# first use clang to check for errors, as its diagnostics are slightly better
+subprocess.check_call(os.path.join(here, "build_debug_clang.bat"))
+
+# get the current list of source files
 v = []
 for s in glob.glob(os.path.join(here, "..", "src", "*.cc")):
     s = os.path.basename(s)
     s = os.path.splitext(s)[0]
     v.append(s)
 
+# compile the current version to assembly
 build(os.path.join(here, "..", "src", "*.cc"))
 for s in v:
     os.replace(s + ".asm", s + "1.asm")
 
+# compile the previous version to assembly
 build(os.path.join(tempfile.gettempdir(), "0", "*.cc"))
 for s in v:
     os.replace(s + ".asm", s + "0.asm")
 
+# compare the assembly output
 for s in v:
     s0 = s + "0.asm"
     s1 = s + "1.asm"
