aef4e2c612f7ce910ba3fe389d9b38d357aa087b
Russell Wallace <russell.wallace@gmail.com>
2023-05-24 03:35:02 +0100

    sort

--- script/python_sort.py
+++ script/python_sort.py
@@ -4,7 +4,7 @@ import re
 
 def end(i):
     for j in range(i, len(u)):
-        if re.match(r"\s*#\s*", u[j]):
+        if re.match(r"\s*#\s*$", u[j]):
             return j
     raise Exception()
 
@@ -13,14 +13,19 @@ here = os.path.dirname(os.path.realpath(__file__))
 for root, dirs, files in os.walk(here):
     for file in files:
         ext = os.path.splitext(file)[1]
-        if ext in (".cc", ".h"):
+        if ext == ".py":
             file = os.path.join(root, file)
 
-            u = open(os.path.join(src, file)).readlines()
+            u = open(file).readlines()
+            old = u.copy()
+
             for i in range(len(u)):
-                if re.match(r"\s*# SORT\s*", u[i]):
-                    j = end(i)
-                    v = u[i:j]
+                if re.match(r"\s*# SORT\s*$", u[i]):
+                    j = end(i + 1)
+                    v = u[i + 1 : j]
                     v.sort()
-                    u[i:j] = v
-            open(os.path.join(src, file), "w", newline="\n").writelines(u)
+                    u[i + 1 : j] = v
+
+            if u != old:
+                print(file)
+                open(file, "w", newline="\n").writelines(u)
