eb3dc2e1642c5a8bce6dde0c9122747354cf80a3
Russell Wallace <russell.wallace@gmail.com>
2023-05-25 10:51:59 +0100

    unpack

---
+++ script/unpack_smtlib2.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+import common
+import smtlib
+
+args = common.args_files()
+os.chdir("C:/smtlib/2")
+
+
+def want(file):
+    ext = os.path.splitext(file)[1]
+    if ext != ".zip":
+        return
+    return 1
+
+
+for arg in args.files:
+    for root, dirs, files in os.walk(arg):
+        for file in files:
+            if want(file):
+                file = os.path.join(root, file)
+                print(file)
+                cmd = "7z", "x", file
+                subprocess.check_call(cmd)
