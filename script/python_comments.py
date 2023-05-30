2d3fec2384c49de6e7d91e38f039f28be50e3cf5
Russell Wallace <russell.wallace@gmail.com>
2023-05-31 00:13:53 +0100

    refactor

--- script/python_comments.py
+++ script/python_comments.py
@@ -6,22 +6,17 @@ import common
 
 def f(v):
     for i in range(len(v)):
-        m = re.match(r"(\s*)# sort$", v[i])
+        m = re.match(r"(\s*)# sort$", v[i], re.IGNORECASE)
         if m:
             v[i] = f"{m[1]}# SORT"
             continue
 
-        m = re.match(r"(\s*)# sortf$", v[i])
+        m = re.match(r"(\s*)# sortf$", v[i], re.IGNORECASE)
         if m:
             v[i] = f"{m[1]}# SORTF"
             continue
 
-        m = re.match(r"(\s*)# todo: (.*)", v[i])
-        if m:
-            v[i] = f"{m[1]}# TODO: {m[2]}"
-            continue
-
-        m = re.match(r"(\s*)# todo:(.*)", v[i])
+        m = re.match(r"(\s*)# todo:\s*(.*)", v[i], re.IGNORECASE)
         if m:
             v[i] = f"{m[1]}# TODO: {m[2]}"
             continue
