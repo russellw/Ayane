41b83aef7734d004725c04075ca2ab346e3a2319
Russell Wallace <russell.wallace@gmail.com>
2023-05-30 11:25:07 +0100

    Wno-switch

--- script/build_debug_clang.bat
+++ script/build_debug_clang.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\LLVM\bin\clang-cl" -DDBG -Feayane -IC:\mpir -MTd -WX -Wimplicit-fallthrough -Wno-deprecated-declarations -Zi -std:c++17 %~dp0..\src\*.cc C:\mpir\debug.lib dbghelp.lib
+"C:\Program Files\LLVM\bin\clang-cl" -DDBG -Feayane -IC:\mpir -MTd -WX -Wimplicit-fallthrough -Wno-deprecated-declarations -Wno-switch -Zi -std:c++17 %~dp0..\src\*.cc C:\mpir\debug.lib dbghelp.lib
