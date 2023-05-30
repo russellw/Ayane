7e3e611e6e3b2124a142ea013464bebbe4987382
Russell Wallace <russell.wallace@gmail.com>
2023-05-30 22:26:09 +0100

    clean

--- script/rename_all.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for root, dirs, files in os.walk("."):
-    for file in files:
-        if "-" in file:
-            file1 = file.replace("-", "_")
-            os.rename(file, file1)
--- script/shuffle.py
+++
@@ -1,9 +0,0 @@
-import random
-import sys
-
-fname = sys.argv[1]
-with open(fname) as f:
-    content = f.readlines()
-random.shuffle(content)
-for s in content:
-    print(s.strip())
